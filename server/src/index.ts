04b2320e54b58966d28375cfc21585328a877636
Luís Filipe <lufimelo@outlook.com>
2025-08-12 14:45:36 -0300

    feat(server): add cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,17 @@
 import express from "express";
 import route from "./routes/routes";
+import cors from "cors";
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 
 app.use(route);
 app.listen(port, () => console.log(`server running on port ${port}`));
